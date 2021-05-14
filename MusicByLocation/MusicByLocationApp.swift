6d49536e5d8472e3a26a7736ce52cae7adada70e
Zach Marinov <Marinov.Z@etoncollege.org.uk>
2021-05-14 09:56:50 +0100

    Initial Commit

---
+++ MusicByLocation/MusicByLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationApp.swift
+//  MusicByLocation
+//
+//  Created by Marinov, Zachary (Coll) on 14/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
